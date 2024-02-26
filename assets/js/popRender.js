72e01a4f4495ba74ffba28c7030b23d92ccaa3c5
albee <vivic5970@gmail.com>
2024-02-26 17:29:07 +0800

    first commit

---
+++ assets/js/popRender.js
@@ -0,0 +1,5 @@
+const alertEvent = document.querySelector('.alert-event')
+// const alertArea = document.querySelector('alert-area')
+// const alertTime = document.querySelector('alert-time')
+
+console.log(alertEvent)
